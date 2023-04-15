3c8f6e5ea919f0e22a63e0682f8c664dd224b93f
FancySnacks <adrian.urbaniak1336@gmail.com>
2023-04-15 11:00:56 +0200

    Widget layout improvements

--- src/photov/GUI/SrcTargetInfo.py
+++ src/photov/GUI/SrcTargetInfo.py
@@ -11,14 +11,22 @@ class SrcTargetInfo:
         self.source_path: StringVar = StringVar(self.root)
         self.target_path: StringVar = StringVar(self.root)
 
+        # ==== Widget Components ==== #
+
         self.FRAME = Frame(self.parent_widget)
         self.FRAME.pack(pady=10)
 
-        self.SRC_FRAME = Frame(self.FRAME)
-        self.SRC_FRAME.grid()
+        self.LABEL_FRAME = Frame(self.FRAME)
+        self.LABEL_FRAME.grid(row=0, column=0)
 
-        self.SRC_LABEL = Label(self.SRC_FRAME, text="SRC")
-        self.SRC_LABEL.grid(row=0, column=0)
+        self.ENTRIES_FRAME = Frame(self.FRAME)
+        self.ENTRIES_FRAME.grid(row=0, column=1)
+
+        self.SRC_LABEL = Label(self.LABEL_FRAME, text="SRC")
+        self.SRC_LABEL.grid(row=0, column=0, sticky="e")
+
+        self.SRC_FRAME = Frame(self.ENTRIES_FRAME)
+        self.SRC_FRAME.grid()
 
         self.SRC_ENTRY = Entry(self.SRC_FRAME, textvariable=self.source_path, width=75)
         self.SRC_ENTRY.grid(padx=5, row=0, column=1)
@@ -26,14 +34,17 @@ class SrcTargetInfo:
         self.SRC_BUTTON = Button(self.SRC_FRAME, text="CHANGE", command=self.change_source)
         self.SRC_BUTTON.grid(row=0, column=2)
 
-        self.TARGET_FRAME = Frame(self.FRAME)
+        self.TARGET_FRAME = Frame(self.ENTRIES_FRAME)
         self.TARGET_FRAME.grid(pady=5)
 
-        self.TARGET_LABEL = Label(self.TARGET_FRAME, text="TARGET")
+        self.TARGET_LABEL = Label(self.LABEL_FRAME, text="TARGET")
         self.TARGET_LABEL.grid(row=1, column=0)
 
         self.TARGET_ENTRY = Entry(self.TARGET_FRAME, textvariable=self.target_path, width=75)
         self.TARGET_ENTRY.grid(padx=5, row=1, column=1)
+
+        self.MOVE_BUTTON = Button(self.TARGET_FRAME, text="COPY")
+        self.MOVE_BUTTON.grid(row=1, column=2)
 
     def set_source_dir(self, path: str):
         self.SRC_ENTRY.delete(0, END)
