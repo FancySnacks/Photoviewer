babeb7143f0d3e687e4c4edd6ecef149a9012792
FancySnacks <adrian.urbaniak1336@gmail.com>
2023-04-16 13:50:12 +0200

    Applied fixes based on flake8 warnings

--- src/photov/GUI/SrcTargetInfo.py
+++ src/photov/GUI/SrcTargetInfo.py
@@ -1,71 +1,71 @@
 import pathlib
 import tkinter.filedialog
-from tkinter import *
+import tkinter
 
 
 class SrcTargetInfo:
-    def __init__(self, root: Tk, parent_widget, parent, image_browser):
-        self.root: Tk = root
+    def __init__(self, root: tkinter.Tk, parent_widget, parent, image_browser):
+        self.root: tkinter.Tk = root
         self.parent_widget = parent_widget
         self.parent = parent
         self.image_browser = image_browser
 
-        self.source_path: StringVar = StringVar(self.root)
-        self.target_path: StringVar = StringVar(self.root)
+        self.source_path: tkinter.StringVar = tkinter.StringVar(self.root)
+        self.target_path: tkinter.StringVar = tkinter.StringVar(self.root)
 
         # ==== Widget Components ==== #
 
-        self.FRAME = Frame(self.parent_widget)
+        self.FRAME = tkinter.Frame(self.parent_widget)
         self.FRAME.pack(pady=10)
 
-        self.LABEL_FRAME = Frame(self.FRAME)
+        self.LABEL_FRAME = tkinter.Frame(self.FRAME)
         self.LABEL_FRAME.grid(row=0, column=0)
 
-        self.ENTRIES_FRAME = Frame(self.FRAME)
+        self.ENTRIES_FRAME = tkinter.Frame(self.FRAME)
         self.ENTRIES_FRAME.grid(row=0, column=1)
 
-        self.SRC_LABEL = Label(self.LABEL_FRAME, text="SRC")
+        self.SRC_LABEL = tkinter.Label(self.LABEL_FRAME, text="SRC")
         self.SRC_LABEL.grid(row=0, column=0, sticky="e")
 
-        self.SRC_FRAME = Frame(self.ENTRIES_FRAME)
+        self.SRC_FRAME = tkinter.Frame(self.ENTRIES_FRAME)
         self.SRC_FRAME.grid()
 
-        self.SRC_ENTRY = Entry(self.SRC_FRAME, textvariable=self.source_path, width=75)
+        self.SRC_ENTRY = tkinter.Entry(self.SRC_FRAME, textvariable=self.source_path, width=75)
         self.SRC_ENTRY.grid(padx=5, row=0, column=1)
 
-        self.SRC_BUTTON = Button(
+        self.SRC_BUTTON = tkinter.Button(
             self.SRC_FRAME, text="Update", command=self.change_source
         )
         self.SRC_BUTTON.grid(row=0, column=2)
 
-        self.BROWSE_SRC_BUTTON = Button(
+        self.BROWSE_SRC_BUTTON = tkinter.Button(
             self.SRC_FRAME, text="Browse..", command=self.browse_source
         )
         self.BROWSE_SRC_BUTTON.grid(row=0, column=3)
 
-        self.TARGET_FRAME = Frame(self.ENTRIES_FRAME)
+        self.TARGET_FRAME = tkinter.Frame(self.ENTRIES_FRAME)
         self.TARGET_FRAME.grid(pady=5)
 
-        self.TARGET_LABEL = Label(self.LABEL_FRAME, text="TARGET")
+        self.TARGET_LABEL = tkinter.Label(self.LABEL_FRAME, text="TARGET")
         self.TARGET_LABEL.grid(row=1, column=0)
 
-        self.TARGET_ENTRY = Entry(
+        self.TARGET_ENTRY = tkinter.Entry(
             self.TARGET_FRAME, textvariable=self.target_path, width=75
         )
         self.TARGET_ENTRY.grid(padx=5, row=1, column=1)
 
-        self.MOVE_BUTTON = Button(
+        self.MOVE_BUTTON = tkinter.Button(
             self.TARGET_FRAME, text="Copy", command=self.copy_file
         )
         self.MOVE_BUTTON.grid(row=1, column=2)
 
-        self.BROWSE_TARGET_BUTTON = Button(
+        self.BROWSE_TARGET_BUTTON = tkinter.Button(
             self.TARGET_FRAME, text="Browse..", command=self.browse_destination
         )
         self.BROWSE_TARGET_BUTTON.grid(row=1, column=3)
 
     def set_source_dir(self, path: str):
-        self.SRC_ENTRY.delete(0, END)
+        self.SRC_ENTRY.delete(0, tkinter.END)
         self.SRC_ENTRY.insert(0, path)
         self.SRC_ENTRY.xview_moveto(len(self.SRC_ENTRY.get()))
 
--- src/photov/__main__.py
+++ src/photov/__main__.py
@@ -8,7 +8,7 @@ def main(argv: Optional[Sequence[str]] | None = None):
     parser = ArgParser(argv)
     work_dir = parser.get_arg("path")
     use_gui = not parser.get_arg("nogui")
-    image_browser = ImageBrowser(show_widget=use_gui, path=work_dir)
+    ImageBrowser(show_widget=use_gui, path=work_dir)
 
 
 if __name__ == "__main__":
