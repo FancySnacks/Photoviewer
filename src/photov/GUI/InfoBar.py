e37e16fec3856574dbceccae07745b520e0b327a
FancySnacks <adrian.urbaniak1336@gmail.com>
2023-04-19 21:33:23 +0200

    Changed tkinter to customtkinter

--- src/photov/GUI/InfoBar.py
+++ src/photov/GUI/InfoBar.py
@@ -1,26 +1,27 @@
-import tkinter
+import customtkinter
+
 from photov.util import bytes_to_nearest_unit
 
 
 class InfoBar:
-    def __init__(self, root: tkinter.Tk, parent_widget, parent):
-        self.root: tkinter.Tk = root
-        self.parent_widget: tkinter.BaseWidget = parent_widget
+    def __init__(self, root: customtkinter.CTk, parent_widget, parent):
+        self.root: customtkinter.CTk = root
+        self.parent_widget = parent_widget
         self.parent: object = parent
 
-        self.FRAME = tkinter.Frame(self.root, borderwidth=2, height=50)
+        self.FRAME = customtkinter.CTkFrame(self.root, height=50)
         self.FRAME.pack(fill="x", expand=True)
 
-        self.INNERFRAME = tkinter.Frame(self.FRAME, borderwidth=2, height=50)
+        self.INNERFRAME = customtkinter.CTkFrame(self.FRAME, height=50)
         self.INNERFRAME.grid(sticky="nsew")
 
-        self.IMG_SIZE_LABEL = tkinter.Label(self.INNERFRAME, text="%x%")
+        self.IMG_SIZE_LABEL = customtkinter.CTkLabel(self.INNERFRAME, text="%x%")
         self.IMG_SIZE_LABEL.grid(row=0, column=0)
 
-        self.SEPARATOR = tkinter.Label(self.INNERFRAME, text=" | ")
+        self.SEPARATOR = customtkinter.CTkLabel(self.INNERFRAME, text=" | ")
         self.SEPARATOR.grid(row=0, column=1)
 
-        self.IMG_FILESIZE_LABEL = tkinter.Label(self.INNERFRAME, text="%")
+        self.IMG_FILESIZE_LABEL = customtkinter.CTkLabel(self.INNERFRAME, text="%")
         self.IMG_FILESIZE_LABEL.grid(row=0, column=2)
 
     def set_img_size_text(self, size: tuple[int, int]):
